35a3c7a34a60a23db3660f40414d17f5c927c1c9
Bhavana <layambhavana21@gmail.com>
2024-12-17 12:02:55 +0530

    Add files via upload

---
+++ MSOffice1.java
@@ -0,0 +1,46 @@
+public class MSOffice extends Thread{
+    public void typing(){
+        try{
+            for(int i=0;i<5;i++){
+                System.out.println("Typing");
+                Thread.sleep(2000);
+            }
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    public void spellcheck(){
+        try{
+            for(int i=0;i<5;i++){
+                System.out.println("spellcheck");
+                Thread.sleep(2000);
+            }
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    public void saving(){
+        try{
+            for(int i=0;i<5;i++){
+                System.out.println("saving");
+                Thread.sleep(2000);
+            }
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    public void run(){
+            if(Thread.currentThread().getName().equals("typing")){
+                typing();
+            }
+            else if(Thread.currentThread().getName().equals("spellcheck")){
+                spellcheck();
+            }
+            else{
+                saving();
+            }
+    }
+}
