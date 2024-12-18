cf5c827830155dfd9d944b7745426c1a8c2d9937
Bhavana <layambhavana21@gmail.com>
2024-12-18 15:51:56 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Sample {
+    public static void main(String[] args) {
+        ArrayList al1=new ArrayList();
+        al1.add(30);
+        al1.add(20);
+        System.out.println(al1);
+    }  
+}
