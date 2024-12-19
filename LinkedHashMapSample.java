5cd39955dfd3442bc370a62f57e1745fb23b84ee
Bhavana <layambhavana21@gmail.com>
2024-12-19 13:58:37 +0530

    Add files via upload

---
+++ LinkedHashMapSample.java
@@ -0,0 +1,16 @@
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedHashMap hm=new LinkedHashMap();
+		hm.put(7, "Dhoni");
+		hm.put(18,"Virat");
+		hm.put(45,"Rohit");
+		hm.put(93,"Burmah");
+		System.out.println(hm);
+	}
+
+}
