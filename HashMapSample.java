44257adfef3dd829a9f2f444649dd0bae46db35a
Bhavana <layambhavana21@gmail.com>
2024-12-19 13:46:31 +0530

    Add files via upload

---
+++ HashMapSample.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class HashMapSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap hm=new HashMap();
+		hm.put(7, "Dhoni");
+		hm.put(18,"Virat");
+		hm.put(45,"Rohit");
+		hm.put(93,"Burmah");
+		hm.put(93,"Burmah");
+		System.out.println(hm);
+	}
+
+}
